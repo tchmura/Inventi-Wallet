cd6ce2834f36caf4a551ffe8aea6793278bc7a80
Tomas Chmura <tomas@chmura.cz>
2019-10-19 23:09:33 +0200

    Initial setup

---
+++ src/pages/TransactionsPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { TransactionsList } from "../components/TransactionsList";
+
+const TransactionsPage = () => {
+  return <TransactionsList />;
+};
+
+export { TransactionsPage };
