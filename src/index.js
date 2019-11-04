933d4d550c70bae84192c098a2cd39ec0d1609fe
Tomas Chmura <tomas@chmura.cz>
2019-11-04 20:40:42 +0100

    Add new route for balances

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BalancesPage } from './pages/BalancesPage/BalancesPage';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route exact path="/" component={App} />
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/balances' component={BalancesPage} />
+    </Switch>
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
---
+++ src/pages/BalancesPage/BalancesPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BalancesPage = () => {
+  return <div>balances</div>;
+};
+
+export { BalancesPage };
