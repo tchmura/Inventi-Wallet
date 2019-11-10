7a70a3fe3676c3375e681e2f2421acdabb6f8ba8
Tomas Chmura <tomas@chmura.cz>
2019-11-10 16:34:54 +0100

    Add uuid package

--- server/wallet-server.js
+++ server/wallet-server.js
@@ -1,11 +1,12 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const KoaBody = require('koa-body');
+const uuid = require('uuidv4').default;
 const app = new Koa();
 
 let transactions = [
   {
-    id: 1,
+    id: uuid(),
     name: 'first transaction',
     orientation: 'IN',
     amount: 485,
@@ -14,7 +15,7 @@ let transactions = [
     date: new Date('2018-03-05').toISOString().substr(0, 10)
   },
   {
-    id: 2,
+    id: uuid(),
     name: 'second transaction',
     orientation: 'OUT',
     amount: 48,
@@ -23,7 +24,7 @@ let transactions = [
     date: new Date('2018-03-09').toISOString().substr(0, 10)
   },
   {
-    id: 3,
+    id: uuid(),
     name: 'third transaction',
     orientation: 'IN',
     amount: 7900679897,
@@ -38,7 +39,7 @@ router.get('/transactions', async ctx => {
   ctx.body = transactions;
 });
 router.put('/transactions/:id', KoaBody(), async ctx => {
-  const id = +ctx.params.id;
+  const id = ctx.params.id;
   const updatedItem = JSON.parse(ctx.request.body);
   const index = transactions.findIndex(i => i.id === id);
   if (index === -1) {
@@ -50,13 +51,13 @@ router.put('/transactions/:id', KoaBody(), async ctx => {
 });
 router.post('/transactions', KoaBody(), async ctx => {
   const newItem = JSON.parse(ctx.request.body);
-  newItem.id = Math.round(Math.random() * 10000 + 1);
+  newItem.id = uuid();
   transactions.push(newItem);
   ctx.body = newItem;
   ctx.status = 201;
 });
 router.delete('/transactions/:id', async ctx => {
-  const id = +ctx.params.id;
+  const id = ctx.params.id;
   transactions = [...transactions.filter(i => i.id !== id)];
   ctx.status = 202;
   ctx.body = {};
